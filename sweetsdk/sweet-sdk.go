301bef30ebc9e6e2e989d99fe1d4876f014d1477
托马斯 <thomas@tuomasideMacBook-Pro.local>
2024-07-23 17:09:19 +0800

    添加测试

--- sweetsdk/sweet-sdk.go
+++ sweetsdk/sweet-sdk.go
@@ -2,14 +2,18 @@ package sweetSdk
 
 // package main
 
-import "github.com/f1748x/tw-sdk/sweetsdk/comm"
+import (
+	"fmt"
+
+	"github.com/f1748x/tw-sdk/sweetsdk/comm"
+)
 
 // var ComManager = &Com{}
 // 获取渠道平台
 
 func NewSweetClient(cm *comm.Com) comm.ComInterface {
 	// 1.检查appid
-
+	fmt.Println("test:", cm.AppId)
 	return cm
 }
 
